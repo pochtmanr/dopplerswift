227ced307047ad142a800e6998fa23f575d79acf
Roman Pochtman <pochtmanrca@gmail.com>
2026-02-27 01:20:30 +0100

    Initial Commit

---
+++ PulseVPN/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PulseVPN
+//
+//  Created by Roman Pochtman on 27.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PulseVPN/PulseVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseVPNApp.swift
+//  PulseVPN
+//
+//  Created by Roman Pochtman on 27.02.26.
+//
+
+import SwiftUI
+
+@main
+struct PulseVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
